67c1e2833d4763f250075b84bfc435813f796fd0
Dan Q Nguyen Phuoc <143023074+aitchfifteen@users.noreply.github.com>
2025-12-22 23:11:33 -0600

    Time: 23 ms (65.11%) | Memory: 18.5 MB (88.66%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        count = 0
+        endOfLine = len(nums) - 1
+        for x in range(len(nums)):
+            if count == 0 and nums[x] == 0 and len(nums)!= 1:
+                return False
+            if nums[x] > count:
+                count = nums[x]
+            if x + count >= endOfLine:
+                return True
+            count -= 1
+        return True
+
+

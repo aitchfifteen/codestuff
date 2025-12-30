aa28560acf5ccd673a804653fb69d80067a1bfb0
Dan Q Nguyen Phuoc <143023074+aitchfifteen@users.noreply.github.com>
2025-12-29 20:17:33 -0600

    Time: 29 ms (21.39%) | Memory: 19.3 MB (33.80%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -5,10 +5,7 @@ class Solution:
         water = 0
         count = 0
 
-        for i in range(len(height)):
-            if i == 0:
-                maxLeft[i] = 0
-            else:
+        for i in range(1, len(height)):
                 maxLeft[i] = max(maxLeft[i - 1], height[i - 1])
         
         for i in range(len(height)-2, -1, -1):
